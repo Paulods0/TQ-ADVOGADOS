f178484db2bca80095566b0aefc4c51884845a53
Paulods0 <pauloluguenda0@gmail.com>
2024-05-30 16:25:10 +0100

    fix:footer image alignment

--- src/components/global/footer.tsx
+++ src/components/global/footer.tsx
@@ -3,64 +3,51 @@ import Container from "./container"
 
 const Footer = () => {
   return (
-    <footer className="w-full flex flex-col mt-16">
-      <section
-        style={{
-          backgroundImage: "url('footer-image.png')",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          height: "100vh",
-          marginTop: "56px",
-          position: "relative",
-        }}
-      >
-        <div className="w-[80%] mx-auto h-1 bg-azul absolute -top-1 left-1/2 -translate-x-1/2" />
+    <footer className="w-full mt-28 flex-col flex justify-end">
+      <section className="h-[560px] relative w-full ">
+        <img
+          src="footer-image-20.png"
+          alt="footer-image"
+          className="absolute w-full h-full inset-0 object-cover"
+        />
 
-        <Container className="flex items-center justify-end h-full">
-          <form className="bg-transparent space-y-8 w-full lg:w-[400px]">
-            <div className="w-full">
-              <h1 className="text-2xl text-azul uppercase font-semibold">
-                fale connosco
-              </h1>
-              <h4 className="text-base text-zinc-500">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              </h4>
-            </div>
-
+        <Container className="relative">
+          <form className="absolute top-14 right-1/2 px-8 lg:px-0 -translate-x-1/2 lg:-translate-x-0 lg:right-0 w-full lg:w-[400px] space-y-4 z-20">
+            <h1 className="text-azul text-xl uppercase font-bold">
+              fale connosco
+            </h1>
+            <h4 className="text-neutral-400 text-sm">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            </h4>
             <input
+              className="w-full border-b border-b-neutral-400 bg-transparent placeholder:text-neutral-400 text-neutral-400 outline-none"
               type="text"
               placeholder="Primeiro nome"
-              className="w-full bg-transparent placeholder:text-zinc-500 text-zinc-500 border-b-2 border-zinc-400 outline-none"
             />
             <input
+              className="w-full border-b border-b-neutral-400 bg-transparent placeholder:text-neutral-400 text-neutral-400 outline-none"
               type="text"
               placeholder="Sobrenome"
-              className="w-full bg-transparent placeholder:text-zinc-500 text-zinc-500 border-b-2 border-zinc-400 outline-none"
             />
-
-            <div className="flex items-center gap-2 w-full">
+            <div className="flex gap-2 w-full">
               <input
-                className="border-b-2 placeholder:text-zinc-500 text-zinc-500 border-zinc-400 bg-transparent w-full outline-none"
+                className="w-full border-b border-b-neutral-400 bg-transparent placeholder:text-neutral-400 text-neutral-400 outline-none"
                 type="number"
-                placeholder="Tel"
+                placeholder="Telefone"
               />
               <input
-                className="border-b-2 placeholder:text-zinc-500 text-zinc-500 border-zinc-400 bg-transparent w-full outline-none"
+                className="w-full border-b border-b-neutral-400 bg-transparent placeholder:text-neutral-400 text-neutral-400 outline-none"
                 type="email"
                 placeholder="Email"
               />
             </div>
-
-            <textarea className="w-full outline-none p-4 placeholder:text-zinc-500 text-zinc-500 bg-zinc-100/70 resize-none rounded-xl border-2 border-zinc-400 h-[200px]" />
-
-            <div className="flex flex-col">
-              <p className="text-zinc-500">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem, nisi.
-              </p>
-
-              <div className="w-full flex items-center justify-between mt-4">
-                <div className="flex items-center gap-4">
+            <textarea className="w-full h-[150px] resize-none p-4 outline-none rounded-xl text-neutral-400 border-2 border-neutral-400 bg-white/70" />
+            <div className="flex flex-col gap-4 w-full">
+              <h4 className="text-neutral-400 text-sm">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h4>
+              <div className="w-full flex justify-between items-center">
+                <div className="flex items-center gap-2">
                   {SOCIAL_MEDIAS.map((item, index) => (
                     <a key={index} className="size-8" href={item.link}>
                       <img src={item.icon} alt="icon" />
@@ -68,7 +55,7 @@ const Footer = () => {
                   ))}
                 </div>
 
-                <button className="px-3 py-2 text-white bg-azul uppercase rounded-lg">
+                <button className="p-2 bg-azul text-white uppercase rounded-lg">
                   saber mais
                 </button>
               </div>
@@ -77,12 +64,12 @@ const Footer = () => {
         </Container>
       </section>
 
-      <section className="w-full h-auto lg:h-24 bg-azul">
-        <Container className="flex items-start gap-4 lg:gap-0 py-4 lg:py-0 lg:items-center lg:flex-row flex-col h-full justify-between text-white text-lg">
+      <section className="w-full bg-azul lg:h-24 z-10 p-4">
+        <Container className="h-full w-full flex-col lg:flex-row gap-4 lg:gap-0 flex items-start lg:items-center justify-between">
           {FOOTER_DOWM.map((data, index) => (
             <div key={index} className="flex items-center gap-2">
               <img src={data.icon} className="size-8" alt="" />
-              <span>{data.text}</span>
+              <span className="text-white">{data.text}</span>
             </div>
           ))}
         </Container>
